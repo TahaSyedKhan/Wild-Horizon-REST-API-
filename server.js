72a429c67788dbb2e4e79a0f30cec3be3c322115
TahaSyedKhan <tahasyedkhan123@gmail.com>
2025-07-16 16:34:17 +0530

    added query params and utility function for filtering data for query params

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import http from 'node:http'
 import { getDataFromDB } from './Database/db.js'
 import { sendJSONResponse } from './utils/sendJSONResponse.js'
 import { sendDataFromPathParam } from './utils/sendDataFromPathParams.js'
+import { sendDataFromQueryParams } from './utils/sendDataFromQueryParams.js'
 
 
 const PORT = 8000
@@ -9,8 +10,15 @@ const PORT = 8000
 const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB()
 
-    if(req.url === '/api' && req.method === 'GET') {
-        sendJSONResponse(res, 200, destinations)
+    const urlObj = new URL(req.url, `http://localhost${req.headers.host}`)
+
+    const queryObj = Object.fromEntries(urlObj.searchParams)
+
+    if(urlObj.pathname === '/api' && req.method === 'GET') {
+
+        let filteredData = sendDataFromQueryParams(destinations, queryObj)
+        sendJSONResponse(res, 200, filteredData)
+        
     }  
     else if(req.url.startsWith('/api/continent') && req.method === 'GET') {
         const continent = req.url.split('/').pop()
